926fb56b935d11717030ca6c013d7b2e65265c73
Nikolas Charette <nikolas@Nikolass-MacBook-Pro.local>
2025-11-19 20:24:56 -0500

    init

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1763600359831";const preCache = ["/images/taichi.png","/images/Hutao%20banner%202.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1763601889977";const preCache = ["/images/taichi.png","/images/Hutao%20banner%202.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
