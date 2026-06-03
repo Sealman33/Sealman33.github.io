44c8095923b74e35582ec38424825c244d169576
Nikolas Charette <nikolas@mac.home>
2026-06-03 02:00:30 -0400

    init

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1780465942900";const preCache = ["/images/taichi.png","/images/Hutao%20banner%202.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780466418510";const preCache = ["/images/taichi.png","/images/Hutao%20banner%202.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
